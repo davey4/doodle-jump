3248dcfb16b1330859f5a25d5b245732020cc923
David Martel <daveymartel@gmail.com>
2020-09-29 13:01:29 -0400

    actual doodler

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   
   function createDoodler() {
       grid.appendChild(doodler)
+      doodler.classList.add('doodler')
   }
   createDoodler()
 })
